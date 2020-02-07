df7129a81e163ec56485552a0d9d235b6d9a0ad4
Ahmad M. Sunono <ahmad.m.snono@gmail.com>
2020-02-07 19:10:31 +0200

    Init

---
+++ src/test/java/edu/najah/potato/SampleTest.java
@@ -0,0 +1,16 @@
+package edu.najah.potato;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class SampleTest {
+	
+	@Test
+	public void SampleTest() {
+		int a = 1;
+		int b = 2;
+		assertTrue(a + b == 3);
+	}
+
+}
